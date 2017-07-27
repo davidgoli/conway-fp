caf446dda47b5d2c6a45a63e6bc047f8570fc673
davidgoli <davigoli@gmail.com>
2017-07-26 18:08:34 -0700

    formatting

--- src/index.js
+++ src/index.js
@@ -15,32 +15,30 @@ import render from './render'
 const rows = 128
 const cols = 128
 
-const wrap = (a, idx) => idx < 0 ? a[a.length + idx] : (idx >= a.length ? a[idx - a.length] : a[idx])
+const wrappedIndex = (length, idx) => idx < 0 ? length + idx : idx >= length ? idx - length : idx
+const wrap = (a, idx) => a[wrappedIndex(a.length, idx)]
 
 const get = (a, r, c) => wrap(wrap(a, r), c)
 
 const neighborTotal = (a, r, c) =>
-  get(a, r-1, c-1) +
-    get(a, r, c-1) +
-    get(a, r+1, c-1) +
-    get(a, r-1, c) +
-    get(a, r+1, c) +
-    get(a, r-1, c+1) +
-    get(a, r, c+1) +
-    get(a, r+1, c+1)
+  get(a, r-1, c-1) + get(a, r, c-1) + get(a, r+1, c-1) +
+    get(a, r-1, c) + get(a, r+1, c) +
+    get(a, r-1, c+1) + get(a, r, c+1) + get(a, r+1, c+1)
+
+const nextCellValue = (previousValue, totalNeighbors) => previousValue ? inRange(2, 4)(totalNeighbors) : totalNeighbors === 3
+
+const getCellValue = curry((previousState, r, c) => {
+  const previousValue = get(previousState, r, c)
+
+  const total = neighborTotal(previousState, r, c)
+
+  return nextCellValue(previousValue, total)
+})
 
 const updateState = (getVal) => times(r => times(getVal(r), cols), rows)
 const gameBoardEl = memoize(() => document.getElementById('game'))
 
 const randomize = () => updateState(r => c => !!random(0, 1))
-
-const getCellValue = curry((previousState, r, c) => {
-  const previousValue = previousState[r][c]
-
-  const total = neighborTotal(previousState, r, c)
-
-  return previousValue ? inRange(2, 4)(total) : total === 3
-})
 
 const nextState = compose(updateState, getCellValue)
 
